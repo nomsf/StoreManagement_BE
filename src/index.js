6d98c60d9b5ae854674d45b7cb21c9dd0ee6b1fc
naufal <naufalfirdaus21@gmail.com>
2023-06-07 14:48:32 +0700

    add route (bread & store)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-const express = require('express');
-const app = express()
-const port = 3010
+// Configure enviroment variable with dotenv and .env
+require('dotenv').config();
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-}) 
+// Setup Application
+const express = require('express');
+const app = express();
+const port = process.env.LOCAL_PORT;
+
+// Middleware: parse request to json
+app.use(express.json())
+
+// Route the request
+const route = require('./routers/router')
+app.use('/', route)
+
+app.listen(port, () => console.log(`Server running on port ${port}`)) 
